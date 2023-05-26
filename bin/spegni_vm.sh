efe8bb7a9ac49f327197ba0e9cab21e698369b81
Riccardo <rickysalerno@protonmail.com>
2023-05-26 12:13:03 +0200

    modifica nel ciclo for

--- bin/spegni_vm.sh
+++ bin/spegni_vm.sh
@@ -7,11 +7,12 @@ function spegni_vm() {
 		 epicode_metasploitable_init
 	 do
 		 sudo virsh shutdown $i && pkill -9 virt-viewer
-		 sleep 10
-		 echo "Necessario eseguire un destroy della vm......."
-		 sleep 3 
-		 sudo virsh destroy epicode_metasploitable_init
 	 done
+
+	sleep 10
+	echo "Necessario eseguire un destroy della vm......."
+	sleep 3 
+	sudo virsh destroy epicode_metasploitable_init
 }
 
 spegni_vm
