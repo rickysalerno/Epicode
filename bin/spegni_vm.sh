2c236b5aef749231ab40fe9a05d197fa7f8ceb36
Riccardo <rickysalerno@protonmail.com>
2023-05-26 11:23:06 +0200

    opzione destroy vm

--- bin/spegni_vm.sh
+++ bin/spegni_vm.sh
@@ -7,6 +7,10 @@ function spegni_vm() {
 		 epicode_metasploitable_init
 	 do
 		 sudo virsh shutdown $i && pkill -9 virt-viewer
+		 sleep 10
+		 echo "Necessario eseguire un destroy della vm......."
+		 sleep 3 
+		 sudo virsh destroy epicode_metasploitable_init
 	 done
 }
 
