b3b943c24553ff02030ba55ef882fd7fe60982f1
Riccardo <rickysalerno@protonmail.com>
2023-05-26 10:48:50 +0200

    rimozione spazi vuoti

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Epicode
 Ciao, spero di fare un bellisimo percorso qui in Epicode.
 
-Dividerò gli esercizi per settimana e numero Es. week1_ex1 etc 
+Dividerò gli esercizi per settimana e numero Es. week1/week1_ex1 etc 
--- accendi_vm.sh
+++ accendi_vm.sh
@@ -3,15 +3,15 @@
 
 function accendi_vm() {
 	for i in epicode_kali_linux_init \
-		 epicode_win7_init 
+		 epicode_win7_init
 	 do
 		 sudo virsh start $i
 	 done
 }
 
 function visualizza_vm() {
-	exec virt-viewer spice://localhost:5900 > /dev/null  & 
-	exec virt-viewer spice://localhost:5901 > /dev/null  & 
+	exec virt-viewer spice://localhost:5900 > /dev/null  &
+	exec virt-viewer spice://localhost:5901 > /dev/null  &
 }
 
 
