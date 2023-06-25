67af7c572a1a836b854b38062e51fa7fcae04f2c
Riccardo <rickysalerno@protonmail.com>
2023-06-25 19:45:05 +0200

    aggiunta funzioni con scanf e non

--- week2/week2_m2_d2/ex1.c
+++ week2/week2_m2_d2/ex1.c
@@ -8,8 +8,31 @@ int area_quadrato(int base, int altezza){
 	return result;
 };
 
+int area_quadrato2(){
+	printf("Area quadrato. \n");
+	int base; 
+	int altezza ;
+	printf("Inserisci il valore della base: ");
+	scanf("%d" , &base);
+	printf("Inserisci il valore dell'altezza: ");
+	scanf("%d" , &altezza);
+	int result = (base * altezza); 
+	printf("L'area del quadrato è %d\n", result);
+	return result;
+};
+
 float area_cerchio(float raggio){
 	float result = (PIGRECO * pow(raggio,2)); 
+	return result;
+};
+
+float area_cerchio2(){
+	printf("Area cerchio. \n");
+	float raggio;
+	printf("Inserisci il valore del raggio: ");
+	scanf("%2f" , &raggio);
+	float result = (PIGRECO * pow(raggio,2)); 
+	printf("L'area del cerchio è %.2f\n", result);
 	return result;
 };
 
@@ -18,13 +41,38 @@ int area_triangolo(float lato, float altezza){
 	return result;
 };
 
+int area_triangolo2(){
+	printf("Area triangolo. \n");
+	float lato; float altezza;
+	printf("Inserisci il valore del lato: ");
+	scanf("%2f" , &lato);
+	printf("Inserisci il valore dell'altezza: ");
+	scanf("%2f" , &altezza);
+	float result = ((lato * altezza) / 2.0); 
+	printf("L'area del triangolo è %.2f\n", result);
+	return result;
+};
+
 int main() {
+	// funzioni senza scanf
 	int a = area_quadrato(5,2);
 	float b = area_cerchio(11.0);
 	float c = area_triangolo(10.0,5.0);
+	// fine funz 
+	
+	printf("Adesso eseguo le funzioni senza scanf....\n");
 	printf("L'area del quadrato è %d\n", a);
 	printf("L'area del cerchio è %.2f\n", b);
 	printf("L'area del triangolo è %.2f\n", c);
+
+	printf("Eseguo le funzioni con scanf.....\n");
+	
+	// funzioni scanf
+	area_quadrato2();
+	area_cerchio2();
+	area_triangolo2();
+	// fine funz 
+	
 
 	return 0 ;
 
