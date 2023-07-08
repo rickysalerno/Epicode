895f9065f630f3fda44d2f159caf4b2d9cbb7089
Riccardo <rickysalerno@protonmail.com>
2023-07-08 20:02:23 +0200

    modifica delle fuznioni

--- week2/week2_m2_d5/lista.py
+++ week2/week2_m2_d5/lista.py
@@ -1,32 +1,19 @@
 #!/usr/bin/env python
 
 
-lista = []
+def funzione_a(lista_a):
+    """Attraverso questa funzione abbiamo in restituzione una lista vuota"""
+    return lista_a
 
 
-def lista_a():
-    """Inserisci una serie di parole che andarnno conteggiate dopo"""
-    print("Inserisci 10 elementi da aggiungere alla lista:   ")
-    while True:
-        lista.append(input("Inserisci elemento:   "))
-        if len(lista) == 10:
-            break
-    print(f"La lista è composta da {len(lista)} elementi")
-    print(f"La lista è composta da {lista}")
+def conteggio_per_indice(lista_b):
+    a = funzione_a(["ciao", "sono", "riccardo","salerno"])
+    for i in enumerate(a):
+        print(f"{i}\n\r")
+    for i in a:
+        lista_b.append(len(i))
+    return lista_b
 
 
-def lista_b():
-    """Conteggio dei numeri di stringhe per indice"""
-    print("Adesso eseguiamo il conteggio degli elementi della lista, e quante parole sono presenti per elemento.\n")
-    count = 1
-    for i in enumerate(lista):
-        print(f"Elemento: {i}\n")
-    for i in range(0, len(lista)):
-        count = 0
-        for j in lista[i]:
-            count+=1
-        print(f"L'elemento {i} è composta da: {count} elementi.")
-
-
-lista_a()
-lista_b()
+conteggio = conteggio_per_indice([])
+print(f"{conteggio}\r\n")
