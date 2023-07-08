cbb67739cd3b4b9217f141e1374ead4c2a17460e
Riccardo <rickysalerno@protonmail.com>
2023-07-08 12:36:26 +0200

    creazioni liste

---
+++ week2/week2_m2_d5/lista.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+lista = []
+
+def lista_a():
+    """Inserisci una serie di parole che andarnno conteggiate dopo"""
+    while True:
+        lista.append(input("Inserisci 10 elementi da aggiungere alla lista:   "))
+        if len(lista) == 10:
+            break
+    print(f"La lista è composta da {len(lista)} elementi")
+    print(f"La lista è composta da {lista}")
+
+
+def lista_b():
+    """Conteggio dei numeri di stringhe per indice"""
+    print("Adesso eseguiamo il conteggio degli elementi della lista, e quante parole sono presenti per elemento.\n")
+    count = 1
+    for i in enumerate(lista):
+        print(f"Elemento: {i}\n")
+    for i in range(0, len(lista)):
+        count = 0
+        for j in lista[i]:
+            count+=1
+        print(f"L'elemento {i} è composta da: {count} elementi.")
+
+
+lista_a()
+lista_b()
