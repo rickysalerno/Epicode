064861c09272b3a462261812c9bf2a22f89887f2
Riccardo <rickysalerno@protonmail.com>
2023-07-09 09:58:45 +0200

    creazione semplice e complessa

--- week2/week2_m2_d5/passwd_generator.py
+++ week2/week2_m2_d5/passwd_generator.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python
 
 
+""" L'applicazione creera un generatore di password """
+
+
 import string
 import random
 
 
-def crea_password(grandezza):
-    caratteri = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+def crea_password():
+    """ Creazione Pass In base alla complessità """
+    complessita = ""
+    while True:
+        complessita = input("Seleziona la tipologia di password:   semplice/complessa\n")
+        if complessita != "semplice" and complessita != "complessa":
+            print("Scelta sbagliata, seleziona tra: semplice e complessa\n")
+            continue
+        else:
+            break
+    match complessita:
+        case "semplice":
+            print(f"Hai generato una tipologia di password {complessita}")
+            print("La password sarà priva di simboli e sarà di 8 caratteri")
+            caratteri = string.ascii_lowercase + string.ascii_uppercase + string.digits
+            grandezza = 8
+        case "complessa":
+            print(f"Hai selezionato una tipologia di password {complessita}")
+            print("La password sarà con simboli e sarà di 20 caratteri")
+            grandezza = 20
+            caratteri = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
     combinazione = "".join(random.choice(caratteri) for i in range(grandezza))
     print(f"\nLa grandezza della password è di: {grandezza} caratteri\n")
     print(f"La password è: {combinazione}\n")
-    return combinazione
 
 
-crea_password(15)
+crea_password()
